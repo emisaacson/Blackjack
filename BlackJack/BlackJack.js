c8ce1a33ba4af78008badde54b54c4cba411ab72
Elliot Isaacson <elliot@emikek.com>
2015-08-10 00:18:19 -0400

    Completed version.
    * Basic tests
    * Don't use object state in the solve mode
    * Faster logging
    * Add memoization

--- BlackJack/BlackJack.js
+++ BlackJack/BlackJack.js
@@ -1,6 +1,7 @@
 ﻿
-
 var Deck = function (Cards) {
+   'use strict';
+
     var _Cards;
 	var Suits = ['♠', '♣', '♥', '♦'];
 	var CardTypes = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
@@ -70,6 +71,8 @@ var Moves = {
 };
 
 var Player = function (BJ) {
+   'use strict';
+
 	return {
 		Score: 0,
 		Cards: [],
@@ -90,12 +93,16 @@ var Player = function (BJ) {
             
             var LegalHands = MyHand.filter(function (c) { return c <= 21; });
             
+            // "Stay" if the player busts
             if (LegalHands.length === 0) {
                 return Moves.Stay;
             }
 
             var MyCurrentBest = LegalHands[LegalHands.length - 1];
-            if (MyCurrentBest >= 17) {
+            
+            // Stay on all 18s and hard 17s
+            if ((LegalHands.length === 1 && MyCurrentBest >= 17) ||
+                (LegalHands.length > 1 && MyCurrentBest >= 18)) {
                 return Moves.Stay;
             }
             if (MyCurrentBest < 12) {
@@ -111,6 +118,8 @@ var Player = function (BJ) {
 };
 
 var BlackJack = function () {
+   'use strict';
+
     var _Deck = Deck();
     
     var IsBrowser = typeof window !== 'undefined';
@@ -135,13 +144,14 @@ var BlackJack = function () {
             if (this.Logging) {
                 if (IsBrowser) {
                     var Panel = document.getElementById("output");
-                    var Tmp = document.createElement("div");
-                    Tmp.innerHTML = message;
-                    setTimeout(function () {
-                        var P = document.createElement("p");
-                        P.innerHTML = Tmp.innerText
-                        Panel.innerHTML += "<p>" + Tmp.innerText + "</p>";
-                    });
+                    var P = document.createElement("div");
+                    if (P.hasOwnProperty("innerContent")) {
+                        P.innerContent = message;
+                    }
+                    else {
+                        P.innerText = message;
+                    }
+                    Panel.appendChild(P);
                 }
                 else {
                     console.log(message);
@@ -205,7 +215,12 @@ var BlackJack = function () {
 			}
 
 			return PossibleHandValues;
-		},
+        },
+        IsSoft: function (Hand) {
+            var DealerLegal = Hand.filter(function (c) { return c <= 21; });
+
+            return DealerLegal.length > 1;
+        },
         PlayHand: function () {
             var i, Card, Move, PlayerHand, DealerHand, PlayerLegal, DealerLegal;
             
@@ -287,10 +302,10 @@ var BlackJack = function () {
 
             // Dealer Plays
             DealerHand = this.GetHandValue(this.Dealer.Cards);
-            // Dealer hits on all 16s and soft 17
-            // TODO: the soft calculation is wrong
-            while ((DealerHand.length === 0 && DealerHand[0] < 17) ||
-                  (DealerHand.length > 0 && DealerHand[DealerHand.length - 1] < 18)) {
+            DealerLegal = DealerHand.filter(function (c) { return c <= 21; });
+            // Hit on all 16s and soft 17s
+            while ((DealerLegal.length === 1 && DealerLegal[0] < 17) ||
+                  (DealerLegal.length > 1 && DealerLegal[DealerLegal.length - 1] < 18)) {
                 this.Log("Dealer hits.");
                 Card = this.Deck.Draw();
                 if (Card) {
@@ -302,6 +317,7 @@ var BlackJack = function () {
                     return false;
                 }
                 DealerHand = this.GetHandValue(this.Dealer.Cards);
+                DealerLegal = DealerHand.filter(function (c) { return c <= 21; });
             }
             
             // Did Dealer bust?
@@ -341,132 +357,187 @@ var BlackJack = function () {
 	return BJ;
 };
 
-var Solver = function () {
-    var BJ = BlackJack();
+var DeterministicPlayer = function (BJ, DeterministicMoves) {
+   'use strict';
 
-    BJ.Solve = function (DealerCards, PlayerCards, PlayerStrategy, RestOfDeck) {
-        var i, Card, Move, CurrentDeck, PlayerHand, DealerHand, PlayerLegal, DealerLegal;
+    var BasePlayer = Player(BJ);
+
+    BasePlayer.GetStrategy = function (DealerCard) {
+        var CardsLeft = BJ.Deck.Cards.length;
+        var StartOfHand = CardsLeft + 2 + this.Cards.length;
         
-        this.Dealer.Cards = DealerCards.splice();
-        this.Player.Cards = PlayerCards.splice();
+        var Hand = BJ.GetHandValue(this.Cards);
         
+        if (Hand[0] > 21) {
+            return Moves.Stay;
+        }
+
+        return DeterministicMoves[StartOfHand][CardsLeft];
+    };
+
+    return BasePlayer;
+};
+
+var Solver = function () {
+   'use strict';
+
+    var BJ = BlackJack();
+    
+    BJ.Memo = [];
+    BJ.Parent = [];
+
+    BJ.Solve = function (DealerCards, PlayerCards, PlayerStrategy, RestOfDeck, StartOfHand, prefix) {
+        var i, Card, CurrentDeck, PlayerHand, DealerHand, PlayerLegal, DealerLegal,
+            ValueOfStay, ValueOfHit, CurrentDealerCards, CurrentPlayerCards;
+
+        CurrentDealerCards = DealerCards.slice();
+        CurrentPlayerCards = PlayerCards.slice();
+        CurrentDeck = RestOfDeck.slice();
+        
+        this.Log(prefix + "Beginning recursion");
+
         // Deal player and dealer 2 cards
-        if (this.Player.Cards.length === 0) {
+        if (CurrentPlayerCards.length === 0) {
+            this.Log(prefix + "Dealing hand");
             for (i = 0; i < 2; i++) {
-                Card = this.Deck.Draw();
+                Card = CurrentDeck.pop();
                 if (Card) {
-                    this.Log("Player got a card: " + Card);
-                    this.Player.Deal(Card);
+                    this.Log(prefix + "Player got a card: " + Card);
+                    CurrentPlayerCards.push(Card);
                 }
                 else {
-                    this.Log("Deck ran out, game over.");
+                    this.Log(prefix + "Deck ran out, game over.");
                     return 0;
                 }
                 
-                Card = this.Deck.Draw();
+                Card = CurrentDeck.pop();
                 if (Card) {
-                    this.Log("Dealer got a card: " + Card);
-                    this.Dealer.Deal(Card);
+                    this.Log(prefix + "Dealer got a card: " + Card);
+                    CurrentDealerCards.push(Card);
                 }
                 else {
-                    this.Log("Deck ran out, game over.");
+                    this.Log(prefix + "Deck ran out, game over.");
                     return 0;
                 }
             }
-        }
-        
-        // Did dealer blackjack?
-        DealerHand = this.GetHandValue(this.Dealer.Cards);
-        if (DealerHand[DealerHand.length - 1] === 21) {
-            this.Log("Dealer got blackjack, player loses a point.");
-            this.Player.Score -= 1;
-            return true;
-        }
-        
-        // Did player blackjack?
-        PlayerHand = this.GetHandValue(this.Player.Cards);
-        if (PlayerHand[PlayerHand.length - 1] === 21) {
-            this.Log("Player got blackjack, player gets 1.5 points.");
-            this.Player.Score += 1.5;
-            return true;
-        }
-        
-        // Player plays
-        Move = this.Player.GetStrategy(this.Dealer.Cards[0]);
-        while (Move !== Moves.Stay) {
-            switch (Move) {
-                case Moves.Hit:
-                    this.Log("Player hits.");
-                    Card = this.Deck.Draw();
-                    if (Card) {
-                        this.Log("Player got a card: " + Card);
-                        this.Player.Deal(Card);
-                    }
-                    else {
-                        this.Log("Deck ran out, game over.");
-                        return false;
-                    }
-                    break;
-                case Moves.DoubleDown:
-                    break;
-                case Moves.Split:
-                    break;
+            
+            // Did dealer blackjack?
+            DealerHand = this.GetHandValue(CurrentDealerCards);
+            if (DealerHand[DealerHand.length - 1] === 21) {
+                this.Log(prefix + "Dealer got blackjack, player loses a point.");
+                return -1 + this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
             }
-            Move = this.Player.GetStrategy(this.Dealer.Cards[0]);
-        }
-        
-        this.Log("Player stays.");
-        
-        // Did player bust?
-        PlayerHand = this.GetHandValue(this.Player.Cards);
-        if (PlayerHand[0] > 21) {
-            this.Log("Players busts. Loses 1 point.");
-            this.Player.Score -= 1;
-            return true;
+            
+            // Did player blackjack?
+            PlayerHand = this.GetHandValue(CurrentPlayerCards);
+            if (PlayerHand[PlayerHand.length - 1] === 21) {
+                this.Log(prefix + "Player got blackjack, player gets 1.5 points.");
+                return 1.5 + this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
+            }
         }
 
-        // Dealer Plays
-        DealerHand = this.GetHandValue(this.Dealer.Cards);
-        // Dealer hits on all 16s and soft 17
-        // TODO: the soft calculation is wrong
-        while ((DealerHand.length === 0 && DealerHand[0] < 17) ||
-              (DealerHand.length > 0 && DealerHand[DealerHand.length - 1] < 18)) {
-            this.Log("Dealer hits.");
-            Card = this.Deck.Draw();
-            if (Card) {
-                this.Log("Dealer got a card: " + Card);
-                this.Dealer.Deal(Card);
+        // Did player bust?
+        PlayerHand = this.GetHandValue(CurrentPlayerCards);
+        if (PlayerHand[0] > 21) {
+            this.Log(prefix + "Players busts. Loses 1 point.");
+            return -1 + this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
+        }
+
+        // Player plays
+        if (PlayerStrategy === null) {
+            if (this.Memo[StartOfHand] && CurrentDeck.length === StartOfHand - 4) {
+                this.Log(prefix + "We've been here before: using memoized value.");
+                return this.Memo[StartOfHand];
+            }
+            ValueOfStay = this.Solve(CurrentDealerCards.slice(), CurrentPlayerCards.slice(), Moves.Stay, CurrentDeck.slice(), StartOfHand, prefix + "-");
+            ValueOfHit = this.Solve(CurrentDealerCards.slice(), CurrentPlayerCards.slice(), Moves.Hit, CurrentDeck.slice(), StartOfHand, prefix + "-");
+            
+            if (!this.Parent[StartOfHand]) {
+                this.Parent[StartOfHand] = [];
+            }
+            
+            if (ValueOfStay >= ValueOfHit) {
+                this.Parent[StartOfHand][CurrentDeck.length] = Moves.Stay;
             }
             else {
-                this.Log("Deck ran out, game over.");
-                return false;
+                this.Parent[StartOfHand][CurrentDeck.length] = Moves.Hit;
             }
-            DealerHand = this.GetHandValue(this.Dealer.Cards);
+
+            if (CurrentDeck.length === StartOfHand - 4) {
+                this.Log(prefix + "Memoizing for later (Start position: " + StartOfHand.toString() +")");
+                this.Memo[StartOfHand] = Math.max(ValueOfHit, ValueOfStay);
+            }
+            return Math.max(ValueOfHit, ValueOfStay);
+        }
+
+        switch (PlayerStrategy) {
+            case Moves.Hit:
+                this.Log(prefix + "Player hits.");
+                Card = CurrentDeck.pop();
+                if (Card) {
+                    this.Log(prefix + "Player got a card: " + Card);
+                    CurrentPlayerCards.push(Card);
+                    return this.Solve(CurrentDealerCards.slice(), CurrentPlayerCards.slice(), null, CurrentDeck.slice(), StartOfHand, prefix + "-");
+                }
+                else {
+                    this.Log(prefix + "Deck ran out, game over.");
+                    return 0;
+                }
+                break;
+            case Moves.DoubleDown:
+                break;
+            case Moves.Split:
+                break;
+            case Moves.Stay:
+                this.Log(prefix + "Player stays.");
+                break;
+            default:
+                throw "Invalid player strategy.";
+        }
+        
+        // Dealer Plays
+        DealerHand = this.GetHandValue(CurrentDealerCards);
+        DealerLegal = DealerHand.filter(function (c) { return c <= 21; });
+        // Hit on all 16s and soft 17s
+        while ((DealerLegal.length === 1 && DealerLegal[0] < 17) ||
+              (DealerLegal.length > 1 && DealerLegal[DealerLegal.length - 1] < 18)) {
+            this.Log(prefix + "Dealer hits.");
+            Card = CurrentDeck.pop();
+            if (Card) {
+                this.Log(prefix + "Dealer got a card: " + Card);
+                CurrentDealerCards.push(Card);
+            }
+            else {
+                this.Log(prefix + "Deck ran out, game over.");
+                return 0;
+            }
+            DealerHand = this.GetHandValue(CurrentDealerCards);
+            DealerLegal = DealerHand.filter(function (c) { return c <= 21; });
         }
         
         // Did Dealer bust?
         if (DealerHand[0] > 21) {
-            this.Log("Dealer busts, player gets a point.");
-            this.Player.Score += 1;
-            return true;
+            this.Log(prefix + "Dealer busts, player gets a point.");
+            return 1 + this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
         }
 
         // Who has the best legal score?
+        PlayerHand = this.GetHandValue(CurrentPlayerCards);
         PlayerLegal = PlayerHand.filter(function (c) { return c <= 21; });
         DealerLegal = DealerHand.filter(function (c) { return c <= 21; });
 
         if (PlayerLegal[PlayerLegal.length - 1] < DealerLegal[DealerLegal.length - 1]) {
-            this.Log("Dealer beats player, player loses a point.");
-            this.Player.Score -= 1;
+            this.Log(prefix + "Dealer beats player, player loses a point.");
+            return -1 + this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
         }
         else if (DealerLegal[DealerLegal.length - 1] < PlayerLegal[PlayerLegal.length - 1]) {
-            this.Log("Player beats dealer, player gets a point.");
-            this.Player.Score += 1;
+            this.Log(prefix + "Player beats dealer, player gets a point.");
+            return 1 + this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
         }
         else {
-            this.Log("Tie, no change in score.");
+            this.Log(prefix + "Tie, no change in score.");
+            return this.Solve([], [], null, CurrentDeck.slice(), CurrentDeck.length, prefix + "-");
         }
-        return true;
     };
 
     return BJ;
@@ -486,4 +557,5 @@ if (typeof exports !== 'undefined') {
     exports.Player = Player;
     exports.Moves = Moves;
     exports.Solver = Solver;
+    exports.DeterministicPlayer = DeterministicPlayer;
 }
--- BlackJack/tests.js
+++ BlackJack/tests.js
@@ -47,3 +47,111 @@ exports['Test Simple Game'] = function () {
     BlackJack.Log("Final Score: " + BlackJack.Player.Score.toString());
 };
 
+exports['Test Solver'] = function () {
+    
+    var Normal = BJ.BlackJack();
+    
+    Normal.Logging = true;
+    Normal.Log("Beginning Standard game.");
+    while (Normal.PlayHand()) {
+        Normal.Log("Next hand.");
+    }
+    Normal.Log("Game complete.");
+    Normal.Log("Final Score: " + Normal.Player.Score.toString());
+
+    var Solver = BJ.Solver();
+    
+    Solver.Logging = true;
+    Solver.Log("Beinning Optimal game.");
+    var result = Solver.Solve([], [], null, Solver.Deck.Cards.slice(), Solver.Deck.Cards.length, "-");
+    Solver.Log("Optimal score: " + result.toString());
+
+    var Checker = BJ.BlackJack();
+    Checker.Logging = true;
+    Checker.Log("Beginning Check Game.");
+    Checker.Player = BJ.DeterministicPlayer(Checker, Solver.Parent);
+    while (Checker.PlayHand()) {
+        Checker.Log("Next hand.");
+    }
+    Checker.Log("Game complete.");
+    Checker.Log("Final Score: " + Checker.Player.Score.toString());
+
+    Assert.ok(Normal.Player.Score <= result);
+    Assert.ok(result === Checker.Player.Score);
+};
+
+exports['Test Solver Small Deck'] = function () {
+    
+	var Suits = ['♠', '♣', '♥', '♦'];
+    var Deck = ['7♦', '9♠', '5♥', 'J♠', 'A♦', 'Q♥', '10♣', 'K♥','7♦', '9♠', '5♥', 'J♠', 'A♦', 'Q♥', '10♣', 'K♥','7♦', '9♠', '5♥', 'J♠', 'A♦', 'Q♥', '10♣', 'K♥', '3♦', 'A♠', '2♥', '8♠', '4♣','4♥'];
+    var Normal = BJ.BlackJack();
+    
+    Normal.Logging = false;
+    Normal.Log("Beginning Standard game.");
+    Normal.Deck.Cards = Deck.slice();
+    while (Normal.PlayHand()) {
+        Normal.Log("Next hand.");
+    }
+    Normal.Log("Game complete.");
+    Normal.Log("Final Score: " + Normal.Player.Score.toString());
+    
+    var Solver = BJ.Solver();
+    
+    Solver.Logging = false;
+    Solver.Log("Beginning Optimal game.");
+    Solver.Deck.Cards = Deck.slice();
+    var result = Solver.Solve([], [], null, Deck.slice(), Deck.length, "-");
+    Solver.Log("Optimal score: " + result.toString());
+    
+    
+    var Checker = BJ.BlackJack();
+    Checker.Logging = false;
+    Checker.Log("Beginning Check Game.");
+    Checker.Deck.Cards = Deck.slice();
+    Checker.Player = BJ.DeterministicPlayer(Checker, Solver.Parent);
+    while (Checker.PlayHand()) {
+        Checker.Log("Next hand.");
+    }
+    Checker.Log("Game complete.");
+    Checker.Log("Final Score: " + Checker.Player.Score.toString());
+
+    Assert.ok(Normal.Player.Score <= result);
+    Assert.ok(result === Checker.Player.Score);
+};
+
+exports['Test Solver Random'] = function () {
+    
+    var Normal = BJ.BlackJack();
+    
+    Normal.Logging = true;
+    Normal.Log("Beginning Standard game.");
+    Normal.Deck.Shuffle();
+    var Deck = Normal.Deck.Cards.slice();
+    while (Normal.PlayHand()) {
+        Normal.Log("Next hand.");
+    }
+    Normal.Log("Game complete.");
+    Normal.Log("Final Score: " + Normal.Player.Score.toString());
+
+    var Solver = BJ.Solver();
+    
+    Solver.Logging = true;
+    Solver.Log("Beginning Optimal game.");
+    var result = Solver.Solve([], [], null, Deck.slice(), Deck.length, "-");
+    Solver.Log("Optimal score: " + result.toString());
+
+
+    var Checker = BJ.BlackJack();
+    Checker.Logging = true;
+    Checker.Log("Beginning Check Game.");
+    Checker.Deck.Cards = Deck.slice();
+    Checker.Player = BJ.DeterministicPlayer(Checker, Solver.Parent);
+    while (Checker.PlayHand()) {
+        Checker.Log("Next hand.");
+    }
+    Checker.Log("Game complete.");
+    Checker.Log("Final Score: " + Checker.Player.Score.toString());
+
+    Assert.ok(Normal.Player.Score <= result);
+    Assert.ok(result === Checker.Player.Score);
+};
